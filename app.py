732b348a318660445a80cc37dd508e74cc5debba
Pete Turnham <peteturnham@gmail.com>
2021-03-04 11:46:17 -0500

    added to app.py

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
+##############################################
+##############################################
+#####Package for Flask Web Deployment
+##############################################
+
+
+##############################################
+# Importing Required Packages and Dependencies 
+##############################################
 import numpy as np
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
-from flask import Flask, jsonify
-
+import pandas as pd
+from flask import Flask, jsonify, request, render_template
+import datetime as dt
 
 #################################################
 # Database Setup
@@ -20,36 +29,60 @@ Base.prepare(engine, reflect=True)
 
 # Save reference to the table
 measurement= Base.classes.measurement
-stations = Base.classes.station
+station = Base.classes.station
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
+api_version = "v1.0"
 
+def base_url():
+    return "api/" + api_version
 
 #################################################
 # Flask Routes
 #################################################
 
 @app.route("/")
-def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/measurements<br/>"
-        f"/api/v1.0/stations"
-    )
+@app.route("/index")
+def welcome_page():
+
+    base_p = f'{request.base_url}api/{api_version}/'
+    api_p = f'/api/{api_version}/'
+    return f'''
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Welcome to the Flask API for the Hawaii Climate</h1>
+<p>This app is version {api_version}. Please use the path {request.base_url}api/{api_version} to get to the endpoints.</p>
+<h2>Headings</h2>
+<ul>
+  <li><a href="{request.base_url}">home</a></li>
+  <li><a href="{base_p}precipitation">precipitation - {api_p}precipitation</a></li>
+  <li><a href="{base_p}stations">stations - {api_p}stations</a></li>
+  <li><a href="{base_p}tobs">tobs - {api_p}tobs</a></li>
+  <li>start - {api_p}[start]</li>
+  <li>start and end - {api_p}[start]/[end]</li>
+</ul>
+</body>
+</html>'''
 
 
-@app.route("/api/v1.0/measurements")
-def names():
+
+#################################################
+#   Path to Precipitation Measurement Data 
+#################################################
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of all passenger names"""
+    """Return a list of all recorded temperatures in Hawaii"""
     # Query all passengers
-    results = session.query(measurement.station, measurement.prcp).all()
+    results = session.query(measurement.date, measurement.prcp).all()
+    
 
     session.close()
 
@@ -57,29 +90,49 @@ def names():
     all_data = list(np.ravel(results))
 
     return jsonify(all_data)
+#################################################
+#   Path to Station Data
+#################################################
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of stations and their related data"""
+    #query for stations in the dataset
+    results = session.query(station.station, station.name).all()
+    # Convert list of tuples into normal list
+    all_data = list(np.ravel(results))
+
+    session.close()
 
 
-# @app.route("/api/v1.0/passengers")
-# def passengers():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
 
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    return jsonify(all_data)
+#################################################
+#   Temperature observations
+#################################################
+@app.route("/api/v1.0/tobs")
+def tobs():
+    #creating a session link to our database
+    session = Session(engine)
+    twelve_months = dt.date(2017,8,23)- dt.timedelta(days=365)
+    data = session.query(measurement.station, measurement.tobs).filter(measurement.date>=twelve_months).\
+        filter_by(station = 'USC00519281')
+    all_tobs = []
+    for station, tobs in data:
+        temp_dict = {}
+        temp_dict["station"]= station
+        temp_dict["temperature observation"]= tobs
+        all_tobs.append(temp_dict)
 
-#     session.close()
 
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_passengers = []
-#     for name, age, sex in results:
-#         passenger_dict = {}
-#         passenger_dict["name"] = name
-#         passenger_dict["age"] = age
-#         passenger_dict["sex"] = sex
-#         all_passengers.append(passenger_dict)
 
-#     return jsonify(all_passengers)
+
+    
+    
+    return jsonify(all_tobs)
 
 
 if __name__ == '__main__':
