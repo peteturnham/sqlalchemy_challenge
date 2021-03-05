7e6365192821a724e0383a6b97a40801e6712a70
Pete Turnham <peteturnham@gmail.com>
2021-03-05 09:15:28 -0500

    finished homework

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 ##############################################
 ##############################################
 #####Package for Flask Web Deployment
@@ -62,7 +69,8 @@ def welcome_page():
   <li><a href="{base_p}precipitation">precipitation - {api_p}precipitation</a></li>
   <li><a href="{base_p}stations">stations - {api_p}stations</a></li>
   <li><a href="{base_p}tobs">tobs - {api_p}tobs</a></li>
-  <li>start - {api_p}[start]</li>
+  <li><a href="{base_p}start">start - {api_p}start</a></li>
+  <li>start - {api_p}start date(Y-M-D)</li>
   <li>start and end - {api_p}[start]/[end]</li>
 </ul>
 </body>
@@ -82,14 +90,18 @@ def precipitation():
     """Return a list of all recorded temperatures in Hawaii"""
     # Query all passengers
     results = session.query(measurement.date, measurement.prcp).all()
-    
-
+    rain = []
+    for date, prcp in results:
+        rain_dict = {}
+        rain_dict["date"] = date
+        rain_dict["precipitation"] = prcp
+        rain.append(rain_dict)
+    return jsonify(rain)
     session.close()
 
-    # Convert list of tuples into normal list
-    all_data = list(np.ravel(results))
+    
 
-    return jsonify(all_data)
+
 #################################################
 #   Path to Station Data
 #################################################
@@ -98,42 +110,101 @@ def precipitation():
 def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
-
+    station = Base.classes.station
     """Return a list of stations and their related data"""
     #query for stations in the dataset
-    results = session.query(station.station, station.name).all()
+    results = session.query(station.name, station.station).all()
     # Convert list of tuples into normal list
-    all_data = list(np.ravel(results))
-
     session.close()
+   
+    
+    station_info = []
+    for name, station in results:
+        stations= {}
+        stations["Station Location "]= name
+        stations["Station"] = station
+        station_info.append(stations)
 
-
-
-    return jsonify(all_data)
+     # close out the query session
+   
+    
+    
+    return jsonify(station_info)
+    
 #################################################
-#   Temperature observations
+#   Path to Temperature Observations
 #################################################
 @app.route("/api/v1.0/tobs")
 def tobs():
     #creating a session link to our database
     session = Session(engine)
     twelve_months = dt.date(2017,8,23)- dt.timedelta(days=365)
-    data = session.query(measurement.station, measurement.tobs).filter(measurement.date>=twelve_months).\
+    data = session.query(measurement.station, measurement.tobs, measurement.date).filter(measurement.date>=twelve_months).\
         filter_by(station = 'USC00519281')
     all_tobs = []
-    for station, tobs in data:
+    for station, tobs, date in data:
         temp_dict = {}
         temp_dict["station"]= station
-        temp_dict["temperature observation"]= tobs
+        temp_dict["temperature observation(fahrenheit)"]= tobs
+        temp_dict["date recorded"]= date
         all_tobs.append(temp_dict)
 
+    # close out the query session
+    session.close()
+    info= "Temperature Recordings from 2016-08-23 to 2017-08-18"
+    the_goods=jsonify (all_tobs)
+    return (the_goods)
 
 
+
+#################################################
+#   Path for start date input
+#################################################
+@app.route("/api/v1.0/<start>")
+
+# defineing a function that takes in a variable
+def temp_search(start):
+    #start session
+    session = Session(engine)
+    #convert object to string, then string to integer
+    converted_date= dt.datetime.strptime(start, "%Y-%m-%d").date()
+    #method to return data back to user
+    temps = [func.min(measurement.tobs),
+            func.avg(measurement.tobs),
+            func.max(measurement.tobs)]
+    #query to supply user input date
+    temp = session.query(*temps).\
+                filter(func.strftime('%Y-%m-%d', measurement.date) >= converted_date).all()
+    return jsonify(
+    f'The minimum temperature(fareinheit) is: {temp[0][0]}'
+    f' The average temperature(fareinheit) is: {temp[0][1]}'
+    f' The maximum temperature(fareinheit) is: {temp[0][2]}'
+    )
+
+#################################################
+#   Path for start/end date input
+#################################################
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start,end):
+    #start session
+    session = Session(engine)
+    #user defined start input
+    converted_date= dt.datetime.strptime(start, "%Y-%m-%d").date()
+    end_converted_date =  dt.datetime.strptime(end, "%Y-%m-%d").date()
+    #method to return data back to user
+    temps = [func.min(measurement.tobs),
+            func.avg(measurement.tobs),
+            func.max(measurement.tobs)]
+    #query to supply user input date
+    temp = session.query(*temps).\
+                filter(func.strftime('%Y-%m-%d', measurement.date) >= converted_date).\
+                    filter(func.strftime('%Y-%m-%d', measurement.date) >= end_converted_date).all()
+    return jsonify(f'The minimum temperature(fareinheit) is: {temp[0][0]}'
+    f' The average temperature(fareinheit) is: {temp[0][1]}'
+    f' The maximum temperature(fareinheit) is: {temp[0][2]}'
+    )
 
     
-    
-    return jsonify(all_tobs)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
